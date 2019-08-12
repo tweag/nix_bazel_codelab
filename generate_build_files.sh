90229c0122ca7d9cb68441efbfe1cda086e13337
Tony Aiuto <aiuto@google.com>
2019-08-12 16:55:10 -0400

    Split generate_workspace out of generate_build_files.sh so that it is easier to test workspace updates without whacking the build files. generate_build_files still does the WORKSPACE generation, so we do not have to change CI.

--- generate_build_files.sh
+++ generate_build_files.sh
@@ -1,7 +1,7 @@
 # This file is used for Bazel CI to write the BUILD files for the project.
 
 # You can use it to see the final solutions but note that it will delete your existing
-# BUILD files if you started working. 
+# BUILD files if you started working.
 
 
 # generate Go BUILD file
@@ -109,95 +109,33 @@ sh_test(
     data = [
         "//go/cmd/server:go-server",
         "//java/src/main/java/bazel/bootcamp:JavaLoggingClient",
-    ], 
+    ],
     tags = ["exclusive"]
 )
 EOF
 
 
-# write WORKSPACE file 
-cat > WORKSPACE <<EOF
-workspace(name = "bootcamp")
-load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+cat > typescript/BUILD <<EOF
+load("@npm_bazel_typescript//:index.bzl", "ts_library", "ts_devserver")
 
-# grpc dependencies
-http_archive(
-    name = "io_grpc_grpc_java",
-    urls = [
-        "https://github.com/grpc/grpc-java/archive/v1.22.1.tar.gz",
+ts_devserver(
+    name = "devserver",
+    bootstrap = ["@npm_bazel_typescript//:protobufjs_bootstrap_scripts"],
+    entry_module = "bootcamp/typescript/app",
+    port = 8088,
+    deps = [":app"],
+)
+
+ts_library(
+    name = "app",
+    srcs = ["app.ts"],
+    deps = [
+        "//proto/logger:logger_ts_proto",
     ],
-    sha256 = "6e63bd6f5a82de0b84c802390adb8661013bad9ebf910ad7e1f3f72b5f798832",
-    strip_prefix = "grpc-java-1.22.1",
-)
-load("@io_grpc_grpc_java//:repositories.bzl", "grpc_java_repositories")
-grpc_java_repositories()
-
-
-# go dependencies
-http_archive(
-    name = "io_bazel_rules_go",
-    urls = [
-        "https://storage.googleapis.com/bazel-mirror/github.com/bazelbuild/rules_go/releases/download/0.19.1/rules_go-0.19.1.tar.gz",
-        "https://github.com/bazelbuild/rules_go/releases/download/0.19.1/rules_go-0.19.1.tar.gz",
-    ],
-    sha256 = "8df59f11fb697743cbb3f26cfb8750395f30471e9eabde0d174c3aebc7a1cd39",
-)
-load("@io_bazel_rules_go//go:deps.bzl", "go_rules_dependencies", "go_register_toolchains")
-go_rules_dependencies()
-go_register_toolchains()
-
-http_archive(
-    name = "bazel_gazelle",
-    urls = [
-        "https://github.com/bazelbuild/bazel-gazelle/releases/download/0.18.1/bazel-gazelle-0.18.1.tar.gz",
-    ],
-    sha256 = "be9296bfd64882e3c08e3283c58fcb461fa6dd3c171764fcc4cf322f60615a9b",
-)
-load("@bazel_gazelle//:deps.bzl", "gazelle_dependencies", "go_repository")
-gazelle_dependencies()
-
-go_repository(
-    name = "org_golang_google_grpc",
-    build_file_proto_mode = "disable",
-    importpath = "google.golang.org/grpc",
-    sum = "h1:J0UbZOIrCAl+fpTOf8YLs4dJo8L/owV4LYVtAXQoPkw=",
-    version = "v1.22.0",
-)
-go_repository(
-    name = "org_golang_x_net",
-    importpath = "golang.org/x/net",
-    sum = "h1:oWX7TPOiFAMXLq8o0ikBYfCJVlRHBcsciT5bXOrH628=",
-    version = "v0.0.0-20190311183353-d8887717615a",
-)
-go_repository(
-    name = "org_golang_x_text",
-    importpath = "golang.org/x/text",
-    sum = "h1:g61tztE5qeGQ89tm6NTjjM9VPIm088od1l6aSorWRWg=",
-    version = "v0.3.0",
 )
 
-# BEGIN: typescript dependencies
-http_archive(
-    name = "build_bazel_rules_nodejs",
-    urls = [
-        "https://github.com/bazelbuild/rules_nodejs/releases/download/0.35.0/rules_nodejs-0.35.0.tar.gz",
-    ],
-    sha256 = "6625259f9f77ef90d795d20df1d0385d9b3ce63b6619325f702b6358abb4ab33",
-)
-
-load("@build_bazel_rules_nodejs//:defs.bzl", "node_repositories", "yarn_install")
-
-yarn_install(
-    name = "npm",
-    package_json = "//typescript:package.json",
-    yarn_lock = "//typescript:yarn.lock",
-)
-
-load("@npm//:install_bazel_dependencies.bzl", "install_bazel_dependencies")
-install_bazel_dependencies()
-
-load("@npm_bazel_typescript//:index.bzl", "ts_setup_workspace")
-ts_setup_workspace()
-# END: typescript dependencies
+exports_files(["tsconfig.json"])
 EOF
+
+# write WORKSPACE file
+${SHELL} ./generate_workspace.sh
