4c0a23cf1b1d3afa4640f59b7ce64c33b67fa820
Solène Rapenne <solene.rapenne@tweag.io>
2022-06-29 12:02:28 +0200

    add a script to work with the solutions files
    This allows the following actions:
    
    - display the solution
    - display a diff with your files if a solution is known
    - replace your file with the solution
    - install all solutions at once if you want to ensure the project builds

--- shell.nix
+++ shell.nix
@@ -1,7 +1,11 @@
 { pkgs ? import ./nix/nixpkgs { } }:
 
+let
+  lab-script = pkgs.writeShellScriptBin "lab" (builtins.readFile ./bin/lab);
+in
 with pkgs;
 
 mkShell {
   buildInputs = [ bazel_4 buildifier buildozer nix ];
+  packages = [ lab-script ];
 }
